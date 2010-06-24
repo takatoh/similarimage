c9a0ad1e8b1c8e60f7a37a798ba4022afe9f847d
takatoh <takatoh.m@gmail.com>
2010-06-25 00:18:24 +0900

    bin/color_hist.rb: --index option.

--- bin/color_hist.rb
+++ bin/color_hist.rb
@@ -2,9 +2,20 @@
 # -*- encoding: utf-8 -*-
 
 require 'similar_image'
+require 'optparse'
 
+options = {}
+opts = OptionParser.new
+opts.on('-i', '--index', 'print to index file.'){options[:index] = true}
+opts.on('-h', '--help', 'show this message.'){ print opts; exit}
+opts.parse!
 
-file = ARGV.shift
-hist = SimilarImage.color_histogram(file)
-puts hist.to_s
-
+if options[:index]
+  ARGV.each do |file|
+    hist = SimilarImage.color_histogram(file)
+    puts "#{file}  #{hist.to_s}"
+  end
+else
+  hist = SimilarImage.color_histogram(ARGV.shift)
+  puts hist.to_s
+end
